2b7e99aa78ae447adcf1a535c5ca98bcfb8bf731
John Anstead-Jaacks <jansteadjaacks@usf.edu>
2025-05-01 00:30:07 -0400

    commit

--- R/NAfix.R
+++ R/NAfix.R
@@ -1,11 +1,12 @@
 #' changes values of "N/A" in character data type columns to regular NA data
 #' @param df A data frame.
+#' @param string a character value where you can put whatever value you want replaced by NA
 #'
-#' @return A data frame with "N/A" values replaced with NA.
+#' @return A data frame with selected strings replaced with NA.
 #'
 #' @examples
-#' df <- data.frame(a = c(1, NA, 3), b = c(NA, 5, 6))
-#' NAfix(df)
+#' df <- data.frame(a = c("lalalala", "N/A", "wawawawawa"), b = c("N/A", "trooolloooloo", "yoohoohoo"))
+#' NAfix(df, strings = c("N/A"))
 #'
 #' @export
 NAfix <- function(df, string = "N/A") {df[] <- lapply(df, function(x)
